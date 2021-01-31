fdf539dc1e41769959ef681a98eab473f0b48c20
Mert <mertuygur02@gmail.com>
2021-01-31 12:35:06 +0300

    modal added and look nice :)

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 import './book.css'
+import Modal from './Modal.js'
 
 const Book = ({ book }) => {
+    const [showModal, setShowModal] = useState(false);
+
+    const toggleModal = () => {
+        setShowModal(!showModal);
+    }
     return (
         <div className="book">
             <h2 className="title"><span>{book.bookName}</span></h2>
@@ -11,7 +17,24 @@ const Book = ({ book }) => {
                     return <p key={el[2]}>{el}</p>
                 })}
             </div>
-            <p className="thoughts"><span>{book.thoughts}</span></p>
+            <button onClick={toggleModal} className="details">DETAILS</button>
+            {
+                showModal ? (
+                    <Modal>
+                        <div className="modal">
+                            <h2 className="modal-h">{book.bookName}</h2>
+                            <h3 className="modal-h">{book.author}</h3>
+                            <div className="characterss">
+                                {book.characters.split(",").map((el) => {
+                                    return <p key={el[2]}>{el}</p>
+                                })}
+                            </div>
+                            <p className="modal-p">{book.thoughts}</p>
+                            <button onClick={toggleModal} className="modal-button">CLOSE</button>
+                        </div>
+                    </Modal>
+                ) : null
+            }
         </div>
     )
 }
