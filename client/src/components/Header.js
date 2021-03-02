eaee38beb5b05c73538bd0e3de19169dcd589ecd
Mert <mertuygur02@gmail.com>
2021-03-02 16:30:22 +0300

    colors

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
 import { NavLink } from "react-router-dom";
-
 const Header = () => {
     return (
         <div className="header">
             <div className="header__items">
-                <div className="header__items__search">
-                    <h2>#</h2>
-                    <input type="text" placeholder="Search User by ID" />
+                <div className="header__items__logo">
+                    <a href="/"><h1>virtual library</h1></a>
                 </div>
-
                 <div className="header__items__nav">
                     <NavLink exact to="/" activeClassName="selected">Home</NavLink>
                 </div>
